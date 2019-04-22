f381753473a81d9070696b2454a7e0e054dfbd33
gmoliveira <gmoliveira@ciandt.com>
2019-04-22 17:06:49 -0300

    Altering script to execute the git config using sudo

--- git_credentials.sh
+++ git_credentials.sh
@@ -3,5 +3,6 @@
 mkdir -p /home/go2
 echo "${GIT_PROTOCOL}://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPOSITORY}" > /home/go2/.git-credentials
 chown -R 1000:1000 /home/go2
-git config --global credential.helper 'store --file=/home/go2/.git-credentials'
+apt-get update && apt-get install sudo -y
+sudo -u go git config --global credential.helper 'store --file=/home/go2/.git-credentials'
 
