899a093f8ccc8ba9b6e2427018a83cfe5b0957b9
gmoliveira <gmoliveira@ciandt.com>
2019-04-22 15:57:40 -0300

    Adding script to generate the git credential file through environment variables

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,7 @@ FROM gocd/gocd-agent-debian-9:v19.2.0
 
 RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
 
+COPY ./git_credentials.sh /home/go2/git_credentials.sh
+
+CMD /home/go2/git_credentials.sh && /docker-entrypoint.sh
+
---
+++ git_credentials.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /home/go2
+echo "${GIT_PROTOCOL}://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_REPOSITORY}" > /home/go2/.git-credentials
+chown -R 1000:1000 /home/go2
+git config --global credential.helper 'store --file=/home/go2/.git-credentials'
+
